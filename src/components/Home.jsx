f666fec30f8da39afb13018d602e4dfd2dd92ef4
immyz1 <imran.uddin@mail.utoronto.ca>
2022-09-25 21:26:21 -0400

    initialized home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {HIArrowNarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen bg-black'>
+        {/* <image src={HIArrowNarrowRight}/> */}
+    </div>
+  )
+}
+
+export default Home
