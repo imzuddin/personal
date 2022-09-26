98c79f8386fd1942b0dd8cf4668632950d5622b6
immyz1 <imran.uddin@mail.utoronto.ca>
2022-09-26 00:39:38 -0400

    inistialized skills component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import PYTHON from '../assets/python.png'
+import MONGO from '../assets/mongo.png'
+import REACT from '../assets/react.png'
+
+const Skills = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills
