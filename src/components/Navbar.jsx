fd3c7f2ff644b6cd3a0ac02dcab1bc99d115cbc2
immyz1 <imran.uddin@mail.utoronto.ca>
2022-09-26 00:01:52 -0400

    removed side bar after a certain window size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
         </ul>
 
         {/* Social Icons */}
-        <div className='flex fixed flex-col top-[35%] left-0'>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
             <ul>
                 {/* Add List item to add links to the side bar */}
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0072b1] rounded'> <a href="" className='flex items-center justify-between w-full text-white'> LinkedIn <FaLinkedin size={40}/> </a> </li>
